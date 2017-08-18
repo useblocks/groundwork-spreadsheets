512a84f07c369b3a323cecc69f7c5e3c764cfdcd
Marco Heinemann <marco.heinemann@useblocks.com>
2017-08-18 15:55:53 +0200

    Improved sheet configuration input
    - type is either integer or string now
    - if int: sheet number must be passend
    - if string: can either be active, first, last or name:<sheet_name>

--- tests/test_sheet_config.py
+++ tests/test_sheet_config.py
@@ -6,29 +6,29 @@ from tests.conftest import EmptyPlugin, _get_test_data_path
 def test_sheet_byIndex(emptyApp):
     plugin = EmptyPlugin(emptyApp)
     workbook_path = _get_test_data_path('test.xlsx')
-    config_path = _get_test_data_path('config_sheet_byIndex.json')
+    config_path = _get_test_data_path('config_sheet_index.json')
     plugin.excel_validation.read_excel(config_path, workbook_path)
 
 
 def test_sheet_byIndex_out_of_range(emptyApp):
     plugin = EmptyPlugin(emptyApp)
     workbook_path = _get_test_data_path('test.xlsx')
-    config_path = _get_test_data_path('config_sheet_byIndex_out_of_range.json')
+    config_path = _get_test_data_path('config_sheet_index_out_of_range.json')
     with pytest.raises(IndexError):
         plugin.excel_validation.read_excel(config_path, workbook_path)
 
 
-def test_sheet_byName(emptyApp):
+def test_sheet_name(emptyApp):
     plugin = EmptyPlugin(emptyApp)
     workbook_path = _get_test_data_path('test.xlsx')
-    config_path = _get_test_data_path('config_sheet_byName.json')
+    config_path = _get_test_data_path('config_sheet_name.json')
     plugin.excel_validation.read_excel(config_path, workbook_path)
 
 
-def test_sheet_byName_not_exist(emptyApp):
+def test_sheet_name_not_exist(emptyApp):
     plugin = EmptyPlugin(emptyApp)
     workbook_path = _get_test_data_path('test.xlsx')
-    config_path = _get_test_data_path('config_sheet_byName_not_exist.json')
+    config_path = _get_test_data_path('config_sheet_name_not_exist.json')
     with pytest.raises(KeyError):
         plugin.excel_validation.read_excel(config_path, workbook_path)
 
