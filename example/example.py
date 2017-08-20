9d89d7e3039231a114269bbd42ba32dc4590d785
Marco Heinemann <marco.heinemann@useblocks.com>
2017-08-20 20:14:39 +0200

    Added an example
    - Groundwork app and plugin
    - Example xlsx
    - Example config.json

---
+++ example/example.py
@@ -0,0 +1,28 @@
+from groundwork_spreadsheets import ExcelValidationPattern
+from groundwork import App
+
+
+def Application():
+    app = App(plugins=[], strict=True)
+    return app
+
+
+class ReadCustomExcel(ExcelValidationPattern):
+    def __init__(self, app, name=None, *args, **kwargs):
+        self.name = name or self.__class__.__name__
+        super(ReadCustomExcel, self).__init__(app, *args, **kwargs)
+
+    def activate(self):
+        pass
+
+    def deactivate(self):
+        pass
+
+if __name__ == '__main__':
+    app = App(plugins=[], strict=True)
+    plugin = ReadCustomExcel(app)
+    data = plugin.excel_validation.read_excel('config.json', 'example.xlsx')
+    for row in data:
+        headers = data[row]
+        for header in headers:
+            print("Row {0}, Header '{1}': {2}".format(row, header, data[row][header]))
