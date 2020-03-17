37a337c6d739d7d15c2f89052f5ca39a45ee0936
Haiyang Zhang <haiyang.zhang@useblocks.com>
2020-03-17 14:34:03 +0100

    Removed importlib for version
    fix importlib compatiablity with pyinstaller

--- groundwork_spreadsheets/__init__.py
+++ groundwork_spreadsheets/__init__.py
@@ -1,14 +1,6 @@
 """
 Makes ExcelValidationPattern available for the package on root level
 """
-try:
-    import importlib_metadata  # Python 3.6
-except ModuleNotFoundError:
-    import importlib.metadata as importlib_metadata  # Python 3.7 and 3.8
-
-__version__ = importlib_metadata.version('groundwork_spreadsheets')
-__summary__ = importlib_metadata.metadata('groundwork_spreadsheets')['Summary']
-
 # F401 imported but unused - it's needed as an API
 from .patterns.ExcelValidationPattern.excel_validation_pattern import ExcelValidationPattern  # noqa F401
 # define importable objects
